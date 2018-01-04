e2304591f26d2547bd098323e0b8397455d6115d
unknown <J0050@JRY.COM>
2018-01-04 13:49:42 +0800

    项目初始化

---
+++ src/main/java/MyFunctionsApplication.java
@@ -0,0 +1,17 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: 孟庆浩
+ * @Description:
+ * @Date: Created in 18-1-4 上午11:37
+ * @Modified By:
+ */
+
+@SpringBootApplication
+public class MyFunctionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFunctionsApplication.class, args);
+    }
+}
